ffb911939f9e9e4fa06271233b7ef4b906c1c7ce
Prathuish-dev <prathuishsanjeevan4602@gmail.com>
2025-07-04 01:24:46 +0530

    Create white_space_elimination.c

---
+++ white_space_elimination.c
@@ -0,0 +1,49 @@
+//elimination of white spaces
+
+#include<stdio.h>
+
+int main()
+{
+    FILE *fp1,*fp2;
+    fp1 = fopen("btree.c","r");
+    fp2 = fopen("output.c","w");
+    char ch;
+    while((ch=fgetc(fp1))!=EOF)
+    {
+        switch(ch)
+        {
+            case ' ':
+            case '\n':
+            case '\t':
+                break;
+            case '/':
+                ch = fgetc(fp1);
+                if(ch=='/')
+                {    
+                    while((ch=fgetc(fp1))!='\n');
+                }
+                else if(ch=='*')
+                {
+                    char ch1=fgetc(fp1);
+                    char ch2=fgetc(fp1);
+                    while(ch1!='*'&& ch2!='/')
+                    {
+                        ch1=ch2;
+                        ch2=fgetc(fp1);
+                    }
+
+                }
+                else
+                {
+                    fputc('/',fp2);
+                    ungetc(ch,fp1);    
+                }
+                break;
+            default :
+                fputc(ch,fp2);    
+                break;
+        }
+    }
+    fclose(fp1);
+    fclose(fp2);
+}
